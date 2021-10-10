83713dbe4c362affc434d1339ac8c0271a1ceec7
csh1603 <91582838+csh1603@users.noreply.github.com>
2021-10-10 15:45:14 +0900

    Add files via upload

---
+++ assignment 8 source.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+char str[100];
+
+void reversestr(char* orig) {
+	int a;
+	int b = strlen(str) - 1 ;
+	for (a = b; a > -1; a--) {
+		printf("%c", str[a]);
+	}
+	orig[b] = '\0';
+}
+
+int main(void) {
+	char* rev = &str[0];
+
+	printf("문자열을 입력하세요 : ");
+	scanf("%s", str);
+
+	reversestr(rev);
+	printf("\n");
+}
