e63b3b3c73dabd4268bee7552ad06acbf4a4edab
csh1603 <91582838+csh1603@users.noreply.github.com>
2021-10-19 00:19:07 +0900

    Update for exam 1 .cpp

--- for exam 1 .cpp
+++ for exam 1 .cpp
@@ -1,10 +1,12 @@
+//문자 한개 입력 받고 입력받은 숫자 만큼 반복하는 프로그램
+
 #include <stdio.h>
 
 int main(void) {
 	char s;
 	int a, b, i, j;
 
-	printf("���� 1���� ���� 2���� �Է����ּ��� : ");
+	printf("문자 1개와 정수 2개를 입력해주세요 : ");
 	scanf("%c %d %d", &s, &a, &b);
 
 	for (i = 0;i < a;i++) {
